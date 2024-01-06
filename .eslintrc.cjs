6f8eb86f9d2ba22d679f805f98763193aad79e9b
merry <k.maria.m.24.05@gmail.com>
2024-01-06 16:31:35 +0200

    finish project config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,47 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'plugin:import/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['prettier', 'react', 'import'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'no-restricted-exports': [
+      'error',
+      { restrictDefaultExports: { defaultFrom: false } },
     ],
+    'no-trailing-spaces': 2,
+    'import/no-extraneous-dependencies': [2, { devDependencies: true }],
+    'import/extensions': [
+      2,
+      {
+        ignorePackages: true,
+        pattern: {
+          js: 'never',
+          jsx: 'never',
+          json: 'never',
+        },
+      },
+    ],
+    'react/no-unknown-property': [2, { ignore: ['css'] }],
+    'react/jsx-filename-extension': [2, { extensions: ['.jsx'] }],
+    'prettier/prettier': 2,
+    'consistent-return': 2,
+    'newline-before-return': 2,
+    'import/prefer-default-export': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
   },
-}
+  settings: {
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
+};
