08b535c94da40b5c2e99d6bde7ad948715582831
merry <k.maria.m.24.05@gmail.com>
2024-01-09 00:15:21 +0200

    Add custom marker item

--- src/components/Marker/Marker.jsx
+++ src/components/Marker/Marker.jsx
@@ -2,6 +2,13 @@ import { useMemo } from 'react';
 import L from 'leaflet';
 import { Marker as LeafletMarker, Popup } from 'react-leaflet';
 
+const markerIcon = L.icon({
+  iconUrl: './house.png',
+  iconSize: [38, 38],
+  iconAnchor: [19, 38],
+  popupAnchor: [0, -38],
+});
+
 // eslint-disable-next-line react/prop-types
 export default function Marker({ coordinates, name, onClick }) {
   const eventHandlers = useMemo(
@@ -17,6 +24,7 @@ export default function Marker({ coordinates, name, onClick }) {
 
   return (
     <LeafletMarker
+      icon={markerIcon}
       // eslint-disable-next-line react/prop-types
       position={[coordinates.lat, coordinates.lng]}
       eventHandlers={eventHandlers}>
